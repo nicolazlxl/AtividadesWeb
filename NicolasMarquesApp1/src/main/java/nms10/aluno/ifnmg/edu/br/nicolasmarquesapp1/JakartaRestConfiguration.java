a92f7ddf6efe9cfec4aa9f069a513279d2d7c842
nicolazlxl <129507617+nicolazlxl@users.noreply.github.com>
2025-09-10 00:12:27 -0300

    Add files via upload

---
+++ NicolasMarquesApp1/src/main/java/nms10/aluno/ifnmg/edu/br/nicolasmarquesapp1/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package nms10.aluno.ifnmg.edu.br.nicolasmarquesapp1;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ NicolasMarquesApp1/src/main/java/nms10/aluno/ifnmg/edu/br/nicolasmarquesapp1/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package nms10.aluno.ifnmg.edu.br.nicolasmarquesapp1.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
